498d10c8ea8184e837e9aba2397f9e32db6dcd47
Matéo Fernandez <mateo.fernandez73@gmail.com>
2025-07-09 18:58:33 +0900

    fix: keep format of the datasets in the scripts

--- computer-vision/scripts/extract_dfire_dataset.sh
+++ computer-vision/scripts/extract_dfire_dataset.sh
@@ -15,24 +15,6 @@ unzip -qo D-Fire.zip -d D-Fire/
 echo "> D-Fire dataset extracted."
 
 cd D-Fire/
-
-# Training data
-echo "Processing training data..."
-mkdir -p labels/train/
-mv train/labels/* labels/train/
-rm -rf train/labels/
-mv train/images/* train/
-rmdir train/images/
-echo "> Training data processed."
-
-# Validation data
-echo "Processing validation data..."
-mkdir -p labels/val/
-mv test/labels/* labels/val/
-rm -rf test/labels/
-mv test/images/* test/
-rmdir test/images/
 mv test/ val/ # renaming test to val
-echo "> Validation data processed."
 
 echo "Done! The D-Fire dataset is now ready for use."
